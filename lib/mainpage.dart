23cca615724a793dc7c0cc93894bd6827ec98a7e
soyuly <soyul8147@gmail.com>
2022-05-07 23:29:37 +0900

    환경수정

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -38,13 +38,67 @@ class _MainPageState extends State<MainPage> {
 
       ),
       body: Container(
-        margin: const EdgeInsets.fromLTRB(13, 10, 13, 0),
-        child: CupertinoSearchTextField(decoration: BoxDecoration(
-          color: Color.fromRGBO(188, 188, 188, 0.54),
-            borderRadius: BorderRadius.circular(10)
-        ),),
+        margin: const EdgeInsets.fromLTRB(13, 0, 0, 0),
+        child: Column(
+          children: const [SizedBox(
+            height: 35,
+            child: SearchBar()
+          ),
+        SizedBox(
+            height: 100,
+            child: History()
+        ),]
+        ),
       ),
-      bottomNavigationBar: BottomAppBar(),
+
     );
   }
 }
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 0, 13, 0),
+      child: CupertinoSearchTextField(decoration: BoxDecoration(
+          color: Color.fromRGBO(188, 188, 188, 0.54),
+          borderRadius: BorderRadius.circular(10)
+      ),),
+    );
+  }
+}
+
+class History extends StatelessWidget {
+  const History({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(2, 7, 0, 0),
+      child: ListView(
+        children: const [Text('History', style: TextStyle(
+          fontSize: 15,
+          color: Color.fromRGBO(0, 16, 72, 0.6),
+          fontFamily: 'AppleSDGothicNeo',
+          fontWeight: FontWeight.bold
+        ),),
+          Text('History', style: TextStyle(
+              fontSize: 15,
+              color: Color.fromRGBO(0, 16, 72, 0.6),
+              fontFamily: 'AppleSDGothicNeo',
+              fontWeight: FontWeight.bold
+          ),)
+
+        ],
+        scrollDirection: Axis.vertical,
+      ),
+    );
+  }
+}
+
