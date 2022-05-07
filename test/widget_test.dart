b9f1e87687cb0907e0966157a9452730f42f7722
정윤수 <78465062+dbstn1325@users.noreply.github.com>
2022-05-08 00:52:51 +0900

    중복 제거

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,22 +1,16 @@
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
-<<<<<<< HEAD
-// utility that Flutter provides. For example, you can send tap and scroll
-=======
+
 // utility in the flutter_test package. For example, you can send tap and scroll
->>>>>>> d02614a7c11b6bb036c186e125cf629b8caeffea
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-<<<<<<< HEAD
-import 'package:practice/main.dart';
-=======
+
 import 'package:gnumap/main.dart';
->>>>>>> d02614a7c11b6bb036c186e125cf629b8caeffea
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
