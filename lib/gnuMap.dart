85e8f17d91fd06d2b2a299c27852703baa4fec6a
DonghyeonKang <donghyeon3372@gmail.com>
2022-05-19 20:06:51 +0900

    Update GNU Map header design

--- lib/gnuMap.dart
+++ lib/gnuMap.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'mainpage.dart';
@@ -10,134 +11,116 @@ class SearchingPath extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.black),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          centerTitle: true,
-          title: Text(
-            "GNU Map",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-          )),
+      appBar: _navigationBar,
       body: SlidingUpPanel(
-        borderRadius: BorderRadius.circular(10.0),
-        minHeight: 100,
-        padding: EdgeInsets.only(left: 10, right: 10),
-        collapsed: Container(
-          width: MediaQuery.of(context).size.width - 40,
-          margin: EdgeInsets.only(left: 20, right: 20),
-          child: Column(
+          borderRadius: BorderRadius.circular(10.0),
+          minHeight: 100,
+          padding: EdgeInsets.only(left: 10, right: 10),
+          collapsed: Container(
+            width: MediaQuery.of(context).size.width - 40,
+            margin: EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 7, bottom: 5),
+                      width: 30,
+                      height: 5,
+                      decoration: BoxDecoration(color: Colors.grey[300]),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          panel: ListView(
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 7, bottom: 5),
-                    width: 30,
-                    height: 5,
-                    decoration: BoxDecoration(color: Colors.grey[300]),
-                  ),
-                ],
+              Container(
+                margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("컴퓨터과학관, 30동",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("소요시간 20분 소요거리 4km"),
+                    Text("경남 진주시 진주대로 501 30동"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        child: Image.asset('assets/cs.jpg',
+                            width: MediaQuery.of(context).size.width - 40,
+                            fit: BoxFit.fitWidth),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("건물 검색",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("1층: 상담실"),
+                    Text("2층: 열람실"),
+                    Text("3층: 308호, 312호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                    Text("4층: 415호, 416호, 409호"),
+                  ],
+                ),
               ),
+              Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        '정보수정요청',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ]),
+              ),
+              SizedBox(
+                height: 20,
+              )
             ],
           ),
-        ),
-        panel: ListView(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 20, left: 20, right: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("컴퓨터과학관, 30동",
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text("소요시간 20분 소요거리 4km"),
-                  Text("경남 진주시 진주대로 501 30동"),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Container(
-                      child: Image.asset('assets/cs.jpg',
-                          width: MediaQuery.of(context).size.width - 40,
-                          fit: BoxFit.fitWidth),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text("건물 검색",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text("1층: 상담실"),
-                  Text("2층: 열람실"),
-                  Text("3층: 308호, 312호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                  Text("4층: 415호, 416호, 409호"),
-                ],
+          body: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: NaverMapTest(),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 20, right: 20),
-              child:
-                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                Text(
-                  '정보수정요청',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ]),
-            ),
-            SizedBox(
-              height: 20,
-            )
-          ],
-        ),
-        body: Center(
-            child: Column(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 20, right: 10),
-              child: SearchBar(),
-            ),
-            Flexible(
-                child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              child: NaverMapTest(),
-            ))
-          ],
-        )),
-      ),
+              Positioned(
+                  child: Container(
+                margin: const EdgeInsets.only(left: 20, right: 10, top: 10),
+                child: SearchBar(),
+              ))
+            ],
+          )),
     );
   }
 }
@@ -153,12 +136,10 @@ class _NaverMapTestState extends State<NaverMapTest> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: NaverMap(
-          onMapCreated: onMapCreated,
-          mapType: _mapType,
-        ),
+    return Container(
+      child: NaverMap(
+        onMapCreated: onMapCreated,
+        mapType: _mapType,
       ),
     );
   }
@@ -168,3 +149,16 @@ class _NaverMapTestState extends State<NaverMapTest> {
     _controller.complete(controller);
   }
 }
+
+var _navigationBar = CupertinoNavigationBar(
+    leading: Align(
+      widthFactor: 1.0,
+      child: Text("< 메인",
+          style: TextStyle(
+              color: CupertinoColors.black, fontFamily: 'GyeonggiMedium')),
+      alignment: Alignment.center,
+    ),
+    middle: Text("GNU Map",
+        style: TextStyle(
+            color: CupertinoColors.black, fontFamily: 'GyeonggiMedium')),
+    backgroundColor: CupertinoColors.white);
